9dade668ba8278e8d2b3c968af03e82dde1a6bea
mango7789 <981653507@qq.com>
2024-05-16 23:35:22 +0800

    pstree: add buildProcessTree

--- os-workbench/pstree/pstree.c
+++ os-workbench/pstree/pstree.c
@@ -50,11 +50,38 @@ void printVersion();
 
 
 int main(int argc, char *argv[]) {
-	for (int i = 0; i < argc; i++) {
-		assert(argv[i]); // C 标准保证
-		printf("argv[%d] = %s\n", i, argv[i]);
+	char opt;
+	int opt_index;
+	struct node *head;
+
+	head = node_malloc();
+	buildProcessTree(head);
+
+	if (argc == 1) {
+		printProcessTree(head, 0, 0);
 	}
-	assert(!argv[argc]); // C 标准保证
+	else {
+		while ((opt = getopt_long(argc, argv, "pnV", longopts, &opt_index)) != -1) {
+			switch (opt) {
+			case 'V':
+				printVersion();
+				break;
+			case 'p':
+				printProcessTree(head, 0, 0);
+				break;
+			case 'n':
+				printProcessTree(head, 0, 1);
+				break;
+			default:
+				printf("Pstree:\n");
+				printf("You can choose options from [-p, -n, -V] or [show-pids, numeric-sort, version].\n");
+				break;
+			}
+		}
+	}
+
+	node_free(head);
+
 	return 0;
 }
 
@@ -64,7 +91,7 @@ int main(int argc, char *argv[]) {
 
 struct node *node_malloc() {
 	struct node *n = (struct node *)malloc(sizeof(struct node));
-	n->ps = (struct process_state){0, { }, 0}; 
+	n->ps = (struct process_state){0, { }, 'S', 0}; 
 	n->next = NULL;
 	n->child = NULL;
 
@@ -95,8 +122,8 @@ int node_insert(struct node *sn, struct node *fn) {
 
 void node_free(struct node *fn) {
 	struct node *p = fn->child;
-	if(p != NULL) {
-		for( ; p != NULL; ) {
+	if (p != NULL) {
+		for ( ; p != NULL; ) {
 			struct node *tmp = p;
 			p = p->next;
 			node_free(p);
@@ -105,3 +132,63 @@ void node_free(struct node *fn) {
 	free(fn);
 }
 
+int isNumber(char *str) {
+	/* If the input string are all numbers, return 1, o.w. return 0 */
+	for (char *c = str; *c != '\0'; c++) {
+		if (isdigit(*c) == 0) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+void buildProcessTree(struct node *head) {
+	DIR *d;
+	struct dirent *dir;
+	FILE *f;
+	char path[100] = "/proc/";
+
+	/* Open the proc directory */
+	d = opendir(path);
+	if (!d) {
+		fprintf(stderr, "Directory %s doesn't exist!\n", path);
+		exit(1);
+	}
+	else {
+		while ((dir = readdir(d)) != NULL) {
+			if (isNumber(dir->d_name)) {
+				strcat(path, dir->d_name);
+				strcat(path, "/stat");
+
+				/* read the `/stat` file of a specific process */
+				f = open(path, "r");
+				if (!f) {
+					fprintf(stderr, "Pstree failed when open file %s!\n", path);
+					continue;
+				}
+				else {
+					struct node *node = node_malloc();
+					fscanf(f, "%d (%[^)]) %c %d", &node->ps.pid, &node->ps.name, &node->ps.state, &node->ps.ppid);
+					node_insert(node, head);
+					fclose(f);
+				}
+
+			}
+		}
+		closedir(d);
+	}
+}
+
+void printProcessTree(struct node *node, int depth, int type) {
+
+}
+
+void printProcess(struct node *node, int type) {
+	
+}
+
+
+void printVersion() {
+	printf("pstree ");
+	fprintf(stdout, VERSION);
+}
