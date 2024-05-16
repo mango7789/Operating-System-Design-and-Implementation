63637c02ae235d1958c504c8342639208e5b7842
mango7789 <981653507@qq.com>
2024-05-16 11:37:13 +0800

    pstree: try node structure

--- os-workbench/pstree/pstree.c
+++ os-workbench/pstree/pstree.c
@@ -1,42 +1,107 @@
 #include <stdio.h>
-#include <assert.h>
-#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <dirent.h>
+#include <ctype.h>
 #include <string.h>
+#include <assert.h>
 
-bool valid_argument(char curr_arg, char *single_args[], char *double_args[]) {
-	
-	// single arguments
-	for (int i = 0; *single_args[i] != '\0'; i++) {
-		if (strcmp(&curr_arg, single_args[i]) == 0) {
-			return true;
-		}
-	}
+#define VERSION "Version: 0.0.1"
 
-	// double arguments
-	for (int i = 0; *double_args[i] != '\0'; i++) {
-		if (strcmp(&curr_arg, double_args[i]) == 0) {
-			return true;
-		}
-	}
+struct process_state {
+	/* define one process of the os */
+	pid_t pid;
+	char name[30];
+	char state;
+	pid_t ppid;
+};
 
-	return false;
-}
+struct node {
+	/* node */
+	struct process_state ps;
+	struct node *next;
+	struct node *child;
+};
+
+/**
+ * The manipulation of nodes
+*/
+struct node *node_malloc();
+int node_insert(struct node *sn, struct node *fn);
+void node_free(struct node *fn);
+
+static struct option longopts[] = {
+	{"show-pids", 0, NULL, 'p'},
+	{"numeric-sort", 0, NULL, 'n'},
+	{"version", 0, NULL, 'V'},
+	{0, 0, 0, 0}
+};
+
+/**
+ * Build and print the process tree
+*/
+
+int isNumber(char *str);
+void buildProcessTree(struct node *head);
+void printProcessTree(struct node *head, int depth, int type);
+void printProcess(struct node *node, int type);
+void printVersion();
+
 
 int main(int argc, char *argv[]) {
-
-  	// set the optional arguments
-	char *single_argument[] = {"-p", "-n", "-V"};
-	char *double_argument[] = {"--show-pids", "--numeric-sort", "--version"};
-
-	// iterate through the argv
-	for (int i = 1; i < argc; i++) {
-		assert(argv[i]);
-		// printf("argv[%d] = %s\n", i, argv[i]);
-		if (valid_argument(*argv[i], single_argument, double_argument) == false) {
-			// fprintf(stderr, "Invalid argument! Please select from ['-p', '-n', '-V', '--show-pids', '--numeric-sort', '--version']");
-			return 1;
-		}
+	for (int i = 0; i < argc; i++) {
+		assert(argv[i]); // C 标准保证
+		printf("argv[%d] = %s\n", i, argv[i]);
 	}
-	assert(!argv[argc]);
+	assert(!argv[argc]); // C 标准保证
 	return 0;
 }
+
+/**
+ * The implementation of the pre-defined functions
+*/
+
+struct node *node_malloc() {
+	struct node *n = (struct node *)malloc(sizeof(struct node));
+	n->ps = (struct process_state){0, { }, 0}; 
+	n->next = NULL;
+	n->child = NULL;
+
+	return n;
+}
+
+int node_insert(struct node *sn, struct node *fn) {
+	struct node *p;
+	if (sn->ps.ppid == fn->ps.pid) {
+		if (fn->child == NULL) {
+			fn->child = sn;
+		} 
+		else {
+			for (p = fn->child; p->next != NULL; p = p->next);
+			p->next = sn;
+		}
+		return 1;
+	}
+	else {
+		for (p = fn->child; p != NULL; p = p->next) {
+			if (node_insert(sn, p)) {
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+void node_free(struct node *fn) {
+	struct node *p = fn->child;
+	if(p != NULL) {
+		for( ; p != NULL; ) {
+			struct node *tmp = p;
+			p = p->next;
+			node_free(p);
+		}
+	}
+	free(fn);
+}
+
